52489f64934620109c5fde8577ecea36fef32daa
Marius <gbhrdt@users.noreply.github.com>
2019-05-08 23:43:30 +0200

    Update send_notification.php

--- examples/send_notification.php
+++ examples/send_notification.php
@@ -4,10 +4,10 @@ require '../src/CleverPush.php';
 
 $cleverPush = new CleverPush('INSERT_PRIVATE_API_KEY_HERE');
 
-print_r($cleverPush->request('POST', '/notification/send'), [
+print_r($cleverPush->request('POST', '/notification/send', [
     'channelId' => '',
     'title' => 'Example',
     'text' => 'Optional Subtitle', // Optional
     'url' => 'https://cleverpush.com',
     'subscriptionId' => '' // Optional, will broadcast to all subscribers if not given
-]);
+]));
